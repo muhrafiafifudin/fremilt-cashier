1a1482ca886c3d96a800d372201d0113f0341d00
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-04-14 23:21:19 +0700

    Progress templating

---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,11 @@
+<!--begin::Javascript-->
+<!--begin::Global Javascript Bundle(used by all pages)-->
+<script src="assets/plugins/global/plugins.bundle.js"></script>
+<script src="assets/js/scripts.bundle.js"></script>
+<!--end::Global Javascript Bundle-->
+<!--begin::Page Custom Javascript(used by this page)-->
+<script src="assets/js/custom/widgets.js"></script>
+<script src="assets/js/custom/modals/create-app.js"></script>
+<script src="assets/js/custom/modals/upgrade-plan.js"></script>
+<!--end::Page Custom Javascript-->
+<!--end::Javascript-->
---
+++ resources/views/includes/style.blade.php
@@ -0,0 +1,4 @@
+<!--begin::Global Stylesheets Bundle(used by all pages)-->
+<link href="assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+<link href="assets/css/style.bundle.css" rel="stylesheet" type="text/css" />
+<!--end::Global Stylesheets Bundle-->
