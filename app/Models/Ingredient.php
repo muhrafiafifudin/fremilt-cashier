f098bb9edcbcebebd0402c964794fb9ec8656e58
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-07-02 15:27:51 +0700

    Update model

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -5,16 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Cart extends Model
+class Ingredient extends Model
 {
     use HasFactory;
 
-    protected $table = 'carts';
+    protected $table = 'ingredients';
 
     protected $guarded = [];
-
-    public function product()
-    {
-        return $this->belongsTo(Product::class, 'product_id', 'id');
-    }
 }
